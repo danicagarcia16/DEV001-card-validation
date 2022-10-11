b9378a0de4839d6b55f5f5abe1f05271cfc3d27c
Danica <danicagarcia1613@gmail.com>
2022-10-11 01:49:03 -0500

    conmmit2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import validator from './validator.js';
+let cartNumber;
+const enmascarar= document.getElementById("númerodetarjeta");
+const validar= document.getElementById("validar");
+console.log(validar)
+validar.addEventListener( "click", function(){
+cartNumber=enmascarar.value
+enmascarar.value=validator.maskify(enmascarar.value)
+console.log(cartNumber)
+validator.isValid(cartNumber)
+if (validator.isValid(cartNumber)){
+    return alert ("tarjeta valida")}
+    else{ 
+    return alert ("tarjeta invalida")
+}
+});
 
-console.log(validator);
+
+/*validator.isValid(enmascarar.value)
+console.log(validator.isValid(enmascarar.value))
+enmascarar.value=validator.maskify(enmascarar.value)*/
+
+
+const tarjeta= document.querySelector("#tarjeta");
+ tarjeta.addEventListener("click", () => {
+ tarjeta.classList.toggle("active");
+});
+
+const numerotarjeta= document.querySelector("#tarjeta.numerodetarjeta");
+const nombretarjeta= document.querySelector("#tarjeta.nombredetarjeta");
+
+númerodetarjeta.addEventListener("keyup",(e)=>{
+let  valortarjeta= e.target.value;
+
+numerotarjeta.textContent= valortarjeta;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,47 @@
-const validator = {
+const validator ={
+ isValid:function(númerodetarjeta) {
+const card =númerodetarjeta .split("");
+  //console.log(card);
+  let cardreverse = card.reverse();
+  //console.log(cardreverse);
+  let nuevoarray= cardreverse;
+console.log(nuevoarray)
+let suma = 0;
+  let number= 0;
+for (let i = 0; i < nuevoarray.length; i++) {
+    number = parseInt(nuevoarray[i]);
+if (i % 2 == 1) {  
+      number = number*2;
+       if (number >= 10) {
+        number= parseInt(number) - 9;
+        console.log(number)} 
+    } 
+    suma+= number;
+    console.log(suma ,'suma')
+  }
+  if((suma)%10 == 0){
+      /*alert("tarjeta valida")*/
+      return true
+    }else {/*alert("tarjeta invalida")*/
+    return false}
+  },
+maskify:function(númerodetarjeta){
+  if (númerodetarjeta.length < 4) return númerodetarjeta;
+	const last4Characters = númerodetarjeta.substr(-4);
+  const maskingCharacters = númerodetarjeta.substr(0, númerodetarjeta.length - 4).replace(/./g, '#');
+  return `${maskingCharacters}${last4Characters}`;
+}
+}
+
+
+
+
+
+
+
+
   // ...
-};
+
+
 
 export default validator;
