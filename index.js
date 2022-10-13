3b7af746915bc02e1c5859574d8a6a3bd8aa6a44
Danica <danicagarcia1613@gmail.com>
2022-10-13 17:51:12 -0500

    Updates

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let cartNumber;
 const enmascarar= document.getElementById("númerodetarjeta");
 const validar= document.getElementById("validar");
 console.log(validar)
-validar.addEventListener( "click", function(e){
-    e.preventDefault()
-    e.stopPropagation()
+validar.addEventListener( "click", function(){
+    /*e.preventDefault()
+    e.stopPropagation()*/
 cartNumber=enmascarar.value
 enmascarar.value=validator.maskify(enmascarar.value)
 console.log(cartNumber)
